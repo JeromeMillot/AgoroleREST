2d802f213228af31e519ae597a2d73a7261bb6f6
JeromeMillot <castelgoulz@gmail.com>
2018-09-17 17:23:31 +0200

    Ajout du builder de JWT, ajout de la méthode de récupération du JWT dans le métier, tests de regexp pour l'ajout de Users

--- src/main/java/fr/agrorole/dnd/metier/UserMetier.java
+++ src/main/java/fr/agrorole/dnd/metier/UserMetier.java
@@ -6,6 +6,7 @@ import org.apache.commons.lang.StringUtils;
 
 import fr.agrorole.dnd.dao.UserDAO;
 import fr.agrorole.dnd.dto.User;
+import fr.agrorole.dnd.outils.JWTParser;
 
 public class UserMetier {
 
@@ -13,6 +14,19 @@ public class UserMetier {
 	
 	public User getUserFromId(String id) {
 		return this.dao.getUser(id);
+	}
+	
+	public String getAuthToken(String id, String pwd) throws Exception {
+		User user = this.dao.getUser(id);
+		if(null == user) {
+			throw new Exception("Nom d'utilisateur incorrect!");
+		}
+		
+		if(user.getPassword() != pwd) {
+			throw new Exception("Mot de passe incorrect");
+		}		
+		
+		return JWTParser.buildJWT(id);
 	}
 	
 	public List<User> getUserList(String listParam) {
@@ -23,7 +37,23 @@ public class UserMetier {
 		}
 	}
 	
-	public void createUser(User user) {
+	public void createUser(User user) throws Exception {
+		if(null == user.getUserName()) {
+			throw new Exception("Nom d\'utilisateur obligatoire.");
+		} else if(!user.getUserName().trim().matches("\\w{3,30}")) {
+			throw new Exception("Le nom d\'utilisateur doit comporter entre 3 et 30 caractères, des caractères alphanumériques et des underscores.");
+		}
 		
+		if (null == user.getPassword()) {
+			throw new Exception("Le mot de passe est obligatoire");
+		} else if (!user.getUserName().trim().matches("\\w{6,30}")) {
+			throw new Exception("Le mot de passe doit comporter entre 6 et 30 caractères, des caractères alphanumériques et des underscores.");
+		}
+		
+		if(null == user.getEmail()) {
+			throw new Exception("L\'email est obligatoire");
+		} else if (!user.getUserName().trim().matches("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$")) {
+			throw new Exception("Ce format d'email est incorrect.");
+		}
 	}
 }
--- src/main/java/fr/agrorole/dnd/outils/JWTParser.java
+++ src/main/java/fr/agrorole/dnd/outils/JWTParser.java
@@ -1,15 +1,19 @@
 package fr.agrorole.dnd.outils;
 
-import java.io.UnsupportedEncodingException;import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-
+import java.io.UnsupportedEncodingException;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Date;
 import java.util.Map;
-
+import java.util.Map.Entry;
 import org.apache.commons.codec.binary.Base64;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTCreator.Builder;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
 
@@ -23,7 +27,17 @@ public class JWTParser {
 		super();		
 	}
 
-
+	public static String buildJWT(String userId) throws IllegalArgumentException, JWTCreationException, UnsupportedEncodingException {		
+		Instant now = Instant.now();
+		Date oneDayMore = Date.from(now.plus(Duration.ofDays(1)));
+		Builder tokenBuilder = JWT.create();
+		tokenBuilder.withSubject(userId);
+		tokenBuilder.withIssuer(issuer);
+		tokenBuilder.withIssuedAt(Date.from(now));
+		tokenBuilder.withExpiresAt(oneDayMore);
+		
+		return tokenBuilder.sign(Algorithm.HMAC256(secret));
+	}
 
 	public static Map<String, Claim> getClaims(String token) throws IllegalArgumentException, UnsupportedEncodingException {	
 		
@@ -46,18 +60,41 @@ public class JWTParser {
 				.build();
 		
 		DecodedJWT decodedJWT = verifier.verify(token);
+		System.out.println(decodedJWT.getHeader());
 		return decodedJWT.getSignature();
 	}
 	
 	public static String checkCredentials(String token) throws IllegalArgumentException, UnsupportedEncodingException {
-		byte[] credentials = Base64.decodeBase64(getClaims(token).get("sub").asString());
-		System.out.println(Base64.encodeBase64("castelgoulz:mdpalacon".getBytes()));
+		String credentials = getClaims(token).get("sub").asString();
+		System.out.println(Base64.encodeBase64("castelgoulz:mdpalacon".getBytes()).toString());
 		System.out.println(credentials);
 		return credentials.toString();
 		
 	}
 	
+	public static Boolean verifyToken(String token) throws IllegalArgumentException, UnsupportedEncodingException {
+		Map<String, Claim> claims = getClaims(token);
+		for(Entry<String, Claim> entry : claims.entrySet()) {
+			if (entry.getKey() == "exp") {
+				long exp = entry.getValue().asDate().getTime();
+				long today = Date.from(Instant.now()).getTime();
+				if(exp>=today) {
+					throw new JWTVerificationException("Le jeton est périmé.");
+				}
+			} else if(entry.getKey() == "iss") {
+				String iss = entry.getValue().asString();
+				if(iss != issuer) {
+					throw new JWTVerificationException("Le fournisseur du jeton est invalide.");
+				}
+			}
+		}
+		return true;
+	}
+	
 	public static void main(String[] args) throws IllegalArgumentException, UnsupportedEncodingException {
-		checkCredentials("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJZMkZ6ZEdWc1oyOTFiSG82YldSd1lXeGhZMjl1IiwibmFtZSI6IkpvaG4gRG9lIiwiaXNzIjoiQWdvcm9sZSIsImlhdCI6MTUxNjIzOTAyMn0.PYyBr8fhIND5R9wEMeF1vvljqtN8natKlt2RkMAkyg0");
+		String jeton = buildJWT("castelgoulz");
+		checkCredentials(jeton);
+		String test = getSignature(jeton);
+		System.out.println(jeton);
 	}
 }
